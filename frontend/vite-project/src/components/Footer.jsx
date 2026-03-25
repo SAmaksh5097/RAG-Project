91c081bf0293298e5e210fd300607563333c8c07
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-03-25 20:31:57 +0530

    minor change

--- frontend/vite-project/src/components/Footer.jsx
+++ frontend/vite-project/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-8 flex justify-center rounded-2xl border border-[#cdbfae] bg-black p-4 text-center">
-      <h2 className="text-white">WebApp made with ❤️ by Samaksh</h2>
+      <h2 className="text-white">WebApp made with ❤️ by SAmaksh</h2>
     </footer>
   )
 }
